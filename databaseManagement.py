e133c330fe09f177f93c2fe778116adef9cfe689
xiongjeffrey <67850643+xiongjeffrey@users.noreply.github.com>
2022-01-22 12:58:25 -0800

    Update databaseManagement.py

--- databaseManagement.py
+++ databaseManagement.py
@@ -1,3 +1,14 @@
+import numpy as np
+
+global database
+database = []
+
+global idNums
+idNums = []
+
+global tagList
+tagList = []
+
 def enter(id, tagList):
     if id >= len(database):
         database.append(tagList)
@@ -6,3 +17,9 @@ def enter(id, tagList):
     else:
         database[id] = [database[id][i] + tagList[i] for i in range(len(database[id]))]
         idNums[id] += 1
+
+def addTag(tagName):
+    tagList.append(tagName)
+    np.append(database, np.zeros((len(database), 1)), axis=1)
+
+    return database
